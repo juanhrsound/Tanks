07cdb9b4a856e90cb424ae655d2d39d2978380f4
Juan Herrera <juanjosehr08@gmail.com>
2023-01-26 12:02:31 -0800

    updating scripts

--- Tanks Game/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Tanks Game/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Unity.Audio;
 
 namespace Complete
 {
@@ -11,7 +12,10 @@ namespace Complete
         public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
         public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
         public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
-        public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
+
+        //JJ ---- added this array
+        public AudioClip[] m_FireClip;                // Audio that plays when each shot is fired.
+
         public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
         public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
@@ -91,11 +95,12 @@ namespace Complete
                 Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
             // Set the shell's velocity to the launch force in the fire position's forward direction.
-            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
+            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
 
             // Change the clip to the firing clip and play it.
-            m_ShootingAudio.clip = m_FireClip;
-            m_ShootingAudio.Play ();
+            //m_ShootingAudio.clip = m_FireClip;
+
+            m_ShootingAudio.PlayOneShot(m_FireClip[Random.Range(0, m_FireClip.Length)]);
 
             // Reset the launch force.  This is a precaution in case of missing button events.
             m_CurrentLaunchForce = m_MinLaunchForce;

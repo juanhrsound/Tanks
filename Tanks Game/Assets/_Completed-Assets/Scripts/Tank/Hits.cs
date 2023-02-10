5c21a14cc05878afbd8a69f90b64edf504e10d8f
Juan Herrera <juanjosehr08@gmail.com>
2023-02-09 22:28:22 -0800

    adding pumpjack sound

--- Tanks Game/Assets/_Completed-Assets/Scripts/Tank/Hits.cs
+++ Tanks Game/Assets/_Completed-Assets/Scripts/Tank/Hits.cs
@@ -5,6 +5,7 @@ using UnityEngine.Audio;
 
 public class Hits : MonoBehaviour
 {
+    public GameObject gameObject;
     //Colliders // Crash
     public AudioSource hits;
     
@@ -16,46 +17,44 @@ public class Hits : MonoBehaviour
     public AudioClip[] m_OilStorage;
     public AudioClip[] m_Buildings;
     public AudioClip[] m_TankRuin;
-    
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.tag == "Tree")
-        {
-            hits.PlayOneShot(m_Trees[Random.Range(0, m_Trees.Length)]);
-        }
-        if (other.tag == "Column")
-        {
-            hits.PlayOneShot(m_Columns[Random.Range(0, m_Columns.Length)]);
-        }
-        if (other.tag == "Pumpjack")
-        {
-            hits.PlayOneShot(m_Pumpjack[Random.Range(0, m_Pumpjack.Length)]);
-        }
-        if (other.tag == "PalmTree")
-        {
-            hits.PlayOneShot(m_PalmTrees[Random.Range(0, m_PalmTrees.Length)]);
-        }
-        if (other.tag == "Radar")
-        {
-            hits.PlayOneShot(m_Radar[Random.Range(0, m_Radar.Length)]);
-        }
-        if (other.tag == "OilStorage")
-        {
-            hits.PlayOneShot(m_OilStorage[Random.Range(0, m_OilStorage.Length)]);
-        }
-        if (other.tag == "Buildings")
-        {
-            hits.PlayOneShot(m_Buildings[Random.Range(0, m_Buildings.Length)]);
-        }
-        if (other.tag == "TankRuin")
+        if (collision.gameObject.tag == "TankRuin")
         {
             hits.PlayOneShot(m_TankRuin[Random.Range(0, m_TankRuin.Length)]);
         }
-       
+        if (collision.gameObject.tag == "Tree")
+        {
+            hits.PlayOneShot(m_Trees[Random.Range(0, m_Trees.Length)]);
+        }
+        if (collision.gameObject.tag == "Column")
+        {
+            hits.PlayOneShot(m_Columns[Random.Range(0, m_Columns.Length)]);
+        }
+        if (collision.gameObject.tag == "Pumpjack")
+        {
+            hits.PlayOneShot(m_Pumpjack[Random.Range(0, m_Pumpjack.Length)]);
+        }
+        if (collision.gameObject.tag == "PalmTree")
+        {
+            hits.PlayOneShot(m_PalmTrees[Random.Range(0, m_PalmTrees.Length)]);
+        }
+        if (collision.gameObject.tag == "Radar")
+        {
+            hits.PlayOneShot(m_Radar[Random.Range(0, m_Radar.Length)]);
+        }
+        if (collision.gameObject.tag == "OilStorage")
+        {
+            hits.PlayOneShot(m_OilStorage[Random.Range(0, m_OilStorage.Length)]);
+        }
+        if (collision.gameObject.tag == "Buildings")
+        {
+            hits.PlayOneShot(m_Buildings[Random.Range(0, m_Buildings.Length)]);
+        }
+    
     }
-
 
 
 }

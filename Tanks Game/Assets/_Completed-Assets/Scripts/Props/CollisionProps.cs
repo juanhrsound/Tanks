fcf9fdffa7b1dda4de0e2b9321d2587e415ec91b
Juan Herrera <juanjosehr08@gmail.com>
2023-02-17 16:59:24 -0800

    adding more scriptable objects

---
+++ Tanks Game/Assets/_Completed-Assets/Scripts/Props/CollisionProps.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionProps : MonoBehaviour
+{
+    public Collisions debrisSound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            AudioComponent();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Shells")
+        {
+            AudioComponent();
+
+        }
+    }
+    private void AudioComponent()
+    {
+        GameObject audioObject = new GameObject();
+        AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(debrisSound.debrisClip[Random.Range(0, debrisSound.debrisClip.Length)]);
+        audioSource.volume = Random.Range(debrisSound.volume.x, debrisSound.volume.y);
+        audioSource.pitch = Random.Range(debrisSound.pitch.x, debrisSound.pitch.y);
+
+
+    }
+
+
+}
